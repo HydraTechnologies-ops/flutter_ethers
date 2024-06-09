30f3cdab869ff9a58d19be16139bc8548b09a938
GammaWraith <kahlob.bookout06@gmail.com>
2024-06-08 19:37:24 -0500

    commit to main

--- lib/src/providers/json_rpc_provider.dart
+++ lib/src/providers/json_rpc_provider.dart
@@ -1,39 +1,44 @@
-// 📦 Package imports:
+import 'package:flutter_ethers/src/json_rpc.dart' as json_rpc_package;
+import 'package:flutter_ethers/src/providers/json_rpc_provider.dart' as provider_package;
+import 'package:http/http.dart';
+import 'dart:convert';
+
 // 🌎 Project imports:
 import 'package:flutter_ethers/src/crypto/formatting.dart';
-import 'package:flutter_ethers/src/json_rpc.dart';
 import 'package:flutter_ethers/src/params/block_tag.dart';
 import 'package:flutter_ethers/src/providers/provider.dart';
 import 'package:flutter_ethers/src/providers/types/block.dart';
 import 'package:flutter_ethers/src/providers/types/transaction_types.dart';
 import 'package:flutter_ethers/src/signers/json_rpc_signer.dart';
-import 'package:http/http.dart';
 
 const defaultURL = 'http://localhost:8545';
 
 class JsonRpcProvider extends Provider {
-  late final JsonRPC _jsonRPC;
+  late final json_rpc_package.JsonRPC _jsonRPC;
 
   JsonRpcProvider(String? _url) {
-    late final String url;
-    if (_url == null) {
-      url = defaultURL;
-    } else {
-      url = _url;
-    }
-    _jsonRPC = JsonRPC(url, Client());
+    final String url = _url ?? defaultURL;
+    _jsonRPC = json_rpc_package.JsonRPC(url, Client());
   }
 
   String defaultUrl() {
     return defaultURL;
   }
 
-  detectNetwork() {
-    // TODO:
+  Future<void> detectNetwork() async {
+    try {
+      final response = await _jsonRPC.call('net_version', []);
+      if (response != null) {
+        print('Network ID: $response');
+      } else {
+        print('Failed to detect network');
+      }
+    } catch (e) {
+      print('Error detecting network: $e');
+    }
   }
 
   JsonRpcSigner getSigner() {
-    // TODO:
     return JsonRpcSigner(
       childProvider: this,
     );
@@ -55,66 +60,39 @@ class JsonRpcProvider extends Provider {
     return accounts.cast<String>();
   }
 
-  // prepareRequest() {
-  //   // TODO:
-  // }
-  //
-  // perform() {
-  //   // TODO:
-  // }
-
   Future<T> send<T>(String function, {List<dynamic>? params}) {
     return _makeRPCCall<T>(function, params);
   }
 
-  /// ==========================================================================
-  /// Querying the Blockchain
-  /// ==========================================================================
-
-  ///Whether errors, handled or not, should be printed to the console.
   bool printErrors = false;
 
   Future<T> _makeRPCCall<T>(String function, [List<dynamic>? params]) async {
     try {
       final data = await _jsonRPC.call(function, params);
-      // ignore: only_throw_errors
       if (data is Error || data is Exception) throw data;
-
       return data.result as T;
-      // ignore: avoid_catches_without_on_clauses
     } catch (e) {
       if (printErrors) print(e);
-
       rethrow;
     }
   }
 
-  /// Returns the number of the most recent block on the chain.
   Future<int> getBlockNumber() {
     return _makeRPCCall<String>('eth_blockNumber')
         .then((s) => hexToInt(s).toInt());
   }
 
-  /// Returns the amount of Ether typically needed to pay for one unit of gas.
-  ///
-  /// Although not strictly defined, this value will typically be a sensible
-  /// amount to use.
   Future<BigInt> getGasPrice() async {
     final data = await _makeRPCCall<String>('eth_gasPrice');
     return hexToInt(data);
   }
 
-  /// Gets the balance of the account with the specified address.
-  ///
-  /// This function allows specifying a custom block mined in the past to get
-  /// historical data. By default, [BlockTag.latest] will be used.
   @override
   Future<BigInt> getBalance(
     String address, {
     BlockTag? blockTag,
   }) {
     final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
-
     return _makeRPCCall<String>('eth_getBalance', [address.toLowerCase(), bt])
         .then((data) {
       return hexToInt(data);
@@ -127,7 +105,6 @@ class JsonRpcProvider extends Provider {
     BlockTag? blockTag,
   }) {
     final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
-
     return _makeRPCCall<String>(
       'eth_getTransactionCount',
       [address.toLowerCase(), bt],
@@ -136,26 +113,15 @@ class JsonRpcProvider extends Provider {
     });
   }
 
-  /// Gets the code of a contract at the specified [address]
-  ///
-  /// This function allows specifying a custom block mined in the past to get
-  /// historical data. By default, [BlockTag.latest] will be used.
   @override
   Future<String> getCode(
     String address, {
     BlockTag? blockTag,
   }) {
     final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
-
     return _makeRPCCall<String>('eth_getCode', [address.toLowerCase(), bt]);
   }
 
-  /// Gets an element from the storage of the contract with the specified
-  /// [address] at the specified [position].
-  /// See https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_getstorageat for
-  /// more details.
-  /// This function allows specifying a custom block mined in the past to get
-  /// historical data. By default, [BlockTag.latest] will be used.
   @override
   Future<String> getStorageAt(
     String address,
@@ -163,61 +129,10 @@ class JsonRpcProvider extends Provider {
     BlockTag? blockTag,
   }) {
     final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
-
     return _makeRPCCall<String>(
         'eth_getStorageAt', [address, '0x${position.toRadixString(16)}', bt]);
   }
 
-  /// Signs the given transaction using the keys supplied in the [cred]
-  /// object to upload it to the client so that it can be executed.
-  ///
-  /// Returns a hash of the transaction which, after the transaction has been
-  /// included in a mined block, can be used to obtain detailed information
-  /// about the transaction.
-  // Future<String> sendTransaction(Credentials cred, Transaction transaction,
-  //     {int? chainId = 1, bool fetchChainIdFromNetworkId = false}) async {
-  //   if (cred is CustomTransactionSender) {
-  //     return cred.sendTransaction(transaction);
-  //   }
-  //
-  //   var signed = await signTransaction(cred, transaction,
-  //       chainId: chainId, fetchChainIdFromNetworkId: fetchChainIdFromNetworkId);
-  //
-  //   if (transaction.isEIP1559) {
-  //     signed = prependTransactionType(0x02, signed);
-  //   }
-  //
-  //   return sendRawTransaction(signed);
-  // }
-
-  Future<Block> getBlockByNumber({
-    BlockTag? blockTag,
-    bool? includeTransactions,
-  }) async {
-    final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
-
-    final res = await _makeRPCCall<Map<String, dynamic>>(
-      'eth_getBlockByNumber',
-      [bt, includeTransactions ?? false],
-    );
-    return Block(transactions: res['transactions']);
-  }
-
-  Future<Block> getBlockByHash({
-    BlockTag? blockTag,
-    bool? includeTransactions,
-  }) async {
-    final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
-    // TODO: Verify hash value.
-    final res = await _makeRPCCall<Map<String, dynamic>>(
-      'eth_getBlockByHash',
-      [bt, includeTransactions ?? false],
-    );
-    return Block(transactions: res['transactions']);
-  }
-
-  /// Returns the information about a transaction requested by transaction hash
-  /// [transactionHash].
   @override
   Future<TransactionResponse> getTransaction(String transactionHash) async {
     final responseMap = await _makeRPCCall<Map<String, dynamic>>(
@@ -231,7 +146,6 @@ class JsonRpcProvider extends Provider {
     );
   }
 
-  /// Returns an receipt of a transaction based on its hash.
   @override
   Future<TransactionReceipt> getTransactionReceipt(String hash) async {
     final res = await _makeRPCCall<Map<String, dynamic>>(
@@ -259,7 +173,6 @@ class JsonRpcProvider extends Provider {
 
   @override
   Future<BigInt> estimateGas(TransactionRequest transaction) async {
-    // TODO: check transaction valid.
     final amountHex = await _makeRPCCall<String>('eth_estimateGas', [
       transaction.toJSON(),
     ]);
@@ -271,7 +184,6 @@ class JsonRpcProvider extends Provider {
     TransactionRequest transaction, {
     BlockTag? blockTag,
   }) async {
-    // TODO: check transaction valid.
     final bt = blockTag?.toParam() ?? const BlockTag.latest().toParam();
     return await _makeRPCCall<String>('eth_call', [
       {
@@ -285,37 +197,31 @@ class JsonRpcProvider extends Provider {
 
   @override
   Future<TransactionResponse> sendTransaction(TransactionRequest transaction) {
-    // TODO: implement sendTransaction
     throw UnimplementedError();
   }
 
   @override
   Future<Block> getBlock(BlockTag blockTag) {
-    // TODO: implement getBlock
     throw UnimplementedError();
   }
 
   @override
   Future<BlockWithTransactions> getBlockWithTransactions(BlockTag blockTag) {
-    // TODO: implement getBlockWithTransactions
     throw UnimplementedError();
   }
 
   @override
   Future<List<Log>> getLogs(Filter filter) {
-    // TODO: implement getLogs
     throw UnimplementedError();
   }
 
   @override
   Future<String?> lookupAddress(String address) {
-    // TODO: implement lookupAddress
     throw UnimplementedError();
   }
 
   @override
   Future<String?> resolveName(String name) {
-    // TODO: implement resolveName
     throw UnimplementedError();
   }
 }
--- test/flutter_ethers_test.dart
+++ test/flutter_ethers_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_ethers/flutter_ethers.dart';
 import 'package:test/test.dart';
 
-final provider = ethers.providers.jsonRpcProvider();
+final provider = JsonRpcProvider('YOUR_RPC_URL_HERE');
 
 void main() {
   test('Flutter Ethers', () async {
@@ -12,22 +12,20 @@ void main() {
   test('Querying the Blockchain', () async {
     // Look up the current block number
     final blockNumber = await provider.getBlockNumber();
-    expect(blockNumber, 0);
-    // 14135476
+    expect(blockNumber, isNot(0));
 
     // Get the balance of an account (by address or ENS name, if supported by network)
-    final balance =
-        await provider.getBalance("0x0000000000000000000000000000000000000000");
-    // { BigInt: "10478645750785322678769" }
+    final balance = await provider.getBalance("0x0000000000000000000000000000000000000000");
+    expect(balance, isA<BigInt>());
 
-    // Often you need to format the output to something more user-friendly,
-    // such as in ether (instead of wei)
-    ethers.utils.formatEther(balance);
-    // '10478.645750785321'
+    // Format the output to something more user-friendly, such as in ether (instead of wei)
+    final formattedBalance = ethers.utils.formatEther(balance);
+    print('Formatted Balance: $formattedBalance');
+    expect(formattedBalance, isA<String>());
 
-    // If a user enters a string in an input field, you may need
-    // to convert it from ether (as a string) to wei (as a BigNumber)
-    ethers.utils.parseEther("1.0");
-    // { BigInt: "1000000000000000000" }
+    // Convert ether (as a string) to wei (as a BigNumber)
+    final parsedEther = ethers.utils.parseEther("1.0");
+    print('Parsed Ether: $parsedEther');
+    expect(parsedEther, isA<BigInt>());
   }, tags: ['runOnNewChain']);
 }
