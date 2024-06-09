e98a8c483b12b4f91b1a4b0a4342a4c3e2728c76
GammaWraith <kahlob.bookout06@gmail.com>
2024-06-08 19:09:16 -0500

    commit to main

--- example/flutter_ethers_example.dart
+++ example/flutter_ethers_example.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_ethers/flutter_ethers.dart';
 
 void main() {
